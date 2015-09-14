5487a50d18679d19560932a9d77ed27923662672
Patriot <patriot@izhevsk.ru>
2015-09-14 14:48:21 +0400

    ticTacToe via Kivy example - first steps

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@ __author__ = 'patriot'
 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
-from kivy.properties import ListProperty
+from kivy.properties import (ListProperty, NumericProperty)
 from kivy.app import App
 
 
@@ -11,6 +11,8 @@ class GridEntry(Button):
 
 
 class TicTacToeGrid(GridLayout):
+    status = ListProperty([0,0,0, 0,0,0, 0,0,0])
+    current_player = NumericProperty(1)
     def __init__(self, *args, **kwargs):
         super(TicTacToeGrid, self).__init__(*args, **kwargs)
 
@@ -22,6 +24,33 @@ class TicTacToeGrid(GridLayout):
 
     def button_pressed(self, button):
         print ('{} button clicked!'.format(button.coords))
+        player = {1: 'O', -1: 'X'}
+        colours = {1: (1,0,0,1), -1: (0,1,0,1)}
+        row, column = button.coords
+
+        status_index = 3*row + column
+        already_played = self.status[status_index]
+
+        if not already_played:
+            self.status[status_index] = self.current_player
+            button.text = {1: 'O', -1: 'X'}[self.current_player]
+            button.background_color = colours[self.current_player]
+            self.current_player *= -1
+
+    def on_status(selfself, instance, new_value):
+        status = new_value
+
+        sums = [sum(status[0:3]),
+                sum(status[3:6]), sum(status[6:9]), sum(status[0::3]),
+                sum(status[1::3]), sum(status[2::3]), sum(status[::4]),
+                sum(status[2:-2:2])]
+
+        if 3 in sums:
+            print("Os win!")
+        elif -3 in sums:
+            print("Xs win!")
+        elif 0 not in self.status:
+            print("Draw!")
 
 class TicTacToeApp(App):
     def build(self):
